99aaeebcd52fb22923680d476496638ed6771039
Célio <celiofagundes30@gmail.com>
2022-05-23 12:07:31 -0300

    styles table

---
+++ components/Table/index.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+import React from 'react'
+import {
+  Status,
+  TableBodyContainer,
+  TableContainer,
+  TableHeaderContainer,
+  Td,
+  Th,
+  Tr,
+} from './styles'
+
+interface MainProps {
+  children?: React.ReactNode
+}
+interface StatusProps {
+  children?: React.ReactNode
+  status?: string | boolean
+}
+interface AvatarProps {
+  url: string
+}
+const Table = (props: MainProps) => {
+  return <TableContainer>{props.children}</TableContainer>
+}
+const TableHeader = (props: MainProps) => {
+  return <TableHeaderContainer>{props.children}</TableHeaderContainer>
+}
+const TableBody = (props: MainProps) => {
+  return <TableBodyContainer>{props.children}</TableBodyContainer>
+}
+const TableRow = (props: MainProps) => {
+  return <Tr>{props.children}</Tr>
+}
+const TableTh = (props: MainProps) => {
+  return <Th>{props.children}</Th>
+}
+const TableTd = (props: StatusProps) => {
+  return <Td status={props.status}>{props.children}</Td>
+}
+const TableStatus = (props: StatusProps) => {
+  return <Status status={props.status}>{props.children}</Status>
+}
+const TableAvatar = (props: AvatarProps) => {
+  return (
+    <Image
+      src={props.url}
+      layout='fixed'
+      height={32}
+      width={32}
+      objectFit='cover'
+      alt='avatar'
+      style={{ borderRadius: '50%'}}
+    />
+  )
+}
+
+Table.Header = TableHeader
+Table.Body = TableBody
+Table.Row = TableRow
+Table.Th = TableTh
+Table.Td = TableTd
+Table.Status = TableStatus
+Table.Avatar = TableAvatar
+export default Table
---
+++ components/Table/styles.ts
@@ -0,0 +1,68 @@
+import styled from '@emotion/styled'
+
+type StatusProps = {
+  status?: string | boolean
+}
+export const TableContainer = styled.table`
+  margin-top: 20px;
+  border-spacing: 0;
+`
+export const TableHeaderContainer = styled.thead``
+export const TableBodyContainer = styled.tbody``
+export const Tr = styled.tr``
+export const Th = styled.th`
+  text-align: left;
+  color: #587169;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 140%;
+  border-top: 1px solid #cad6d1;
+  border-bottom: 1px solid #cad6d1;
+  &:nth-of-type(1) {
+    border-top-left-radius: 8px;
+    border-left: 1px solid #cad6d1;
+  }
+  &:nth-of-type(6) {
+    border-top-right-radius: 8px;
+    border-right: 1px solid #cad6d1;
+  }
+
+  padding: 16px;
+`
+export const Td = styled.td<StatusProps>`
+  position: relative;
+  &:nth-of-type(1) {
+    display: grid;
+    grid-template-columns: 0.3fr 1fr;
+    grid-gap:0;
+    align-items: center;
+    font-weight: 500;
+  }
+  color: ${props => (props.status === 'active' ? '#587169' : '#A3B8B0')};
+  border-bottom: 1px solid #eaefed;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 140%;
+  padding: 16px;
+`
+
+export const Status = styled.div<StatusProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 116%;
+  padding: 4px 8px;
+  border-radius: 80px;
+  text-transform: capitalize;
+  background-color: ${props => (props.status === 'inactive' ? '#EAEFED' : '#B5F1DD')};
+  color: ${props => (props.status === 'inactive' ? '#A3B8B0' : '#587169')};
+`
